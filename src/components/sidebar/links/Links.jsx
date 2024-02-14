562264947dc781c4be18a0e7bc1231bb032fb211
VishnuPratapGit <vishnupratap898@gmail.com>
2024-02-14 11:46:42 +0530

    include whileHover in Links

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -31,7 +31,7 @@ const Links = () => {
     return (
         <motion.div className="links" variants={variants}>
             {items.map((item) => (
-                <motion.a href={`#${item}`} key={item} variants={itemVariants}>
+                <motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
                     {item}
                 </motion.a>
             ))}
